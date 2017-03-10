649820adaf7c043d72f0988105613ff0ac52f13e
Oliver Eder <eder@essential-data.sk>
2017-03-10 13:42:45 +0100

    Modified for HaikuMaster logic.

--- android/HaikuMasterAndroid/app/src/main/java/com/haiku/oliver/haikumasterandroid/MainActivity.java
+++ android/HaikuMasterAndroid/app/src/main/java/com/haiku/oliver/haikumasterandroid/MainActivity.java
@@ -1,20 +1,4 @@
-/*
- * Copyright 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.google.sample.cloudvision;
+package com.haiku.oliver.haikumasterandroid;
 
 import android.Manifest;
 import android.content.DialogInterface;
@@ -22,7 +6,6 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
@@ -30,6 +13,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
@@ -65,6 +49,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
     private static final String CLOUD_VISION_API_KEY = "AIzaSyD94Z8sZC46f4i8aviM1EAMcAedGl9AQMA";
     public static final String FILE_NAME = "temp.jpg";
     private static final String ANDROID_CERT_HEADER = "X-Android-Cert";
